1d6a6919bdc0a1e3ea466f3920fd08ea412670ab
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2014-11-20 23:35:53 +0000

    Scrutinizer Auto-Fixes
    This patch was automatically generated as part of the following inspection:
    https://scrutinizer-ci.com/g/richardmiller/PhpSpecExtension/inspections/e7d38f6f-b6b2-4869-a3e6-66d195ba2cea
    
    Enabled analysis tools:
     - PHP Analyzer
     - PHP PDepend
     - PHP Similarity Analyzer
     - PHP Change Tracking Analyzer

--- src/Process/CommandRunner.php
+++ src/Process/CommandRunner.php
@@ -4,7 +4,15 @@ namespace RMiller\PhpSpecExtension\Process;
 
 interface CommandRunner
 {
+    /**
+     * @param string|false $path
+     *
+     * @return void
+     */
     public function runCommand($path, $args);
 
+    /**
+     * @return boolean
+     */
     public function isSupported();
 }
